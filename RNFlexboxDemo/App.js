a165c1c0bc77d303b74064ff5ae770b6805d467e
Harvey <rqp2011@gmail.com>
2018-11-12 15:43:32 +0800

    add flexbox demo

---
+++ RNFlexboxDemo/App.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+
+type Props = {};
+export default class App extends Component<Props> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.view1style}/>
+                <View style={styles.view2style}/>
+                <View style={styles.view3style}/>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        marginTop: 40,
+        flexDirection: 'row',
+        alignItems:'stretch',
+        backgroundColor: '#F5FCFF',
+    },
+    view1style: {
+        fontSize: 20,
+        textAlign: 'center',
+        width: 50,
+        backgroundColor: '#00dd00'
+    },
+    view2style: {
+        textAlign: 'center',
+        color: '#333333',
+        backgroundColor: '#dddd00',
+        width: 50,
+        height: 50
+    },
+    view3style: {
+        textAlign: 'center',
+        color: '#333333',
+        backgroundColor: '#00dddd',
+        width: 50,
+        height: 50
+    },
+});
